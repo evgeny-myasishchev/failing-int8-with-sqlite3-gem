6eade56a0ed4e172d82e54ebc86d6575ba3167f8
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2011-11-13 12:49:46 +0200

    Implementing native sample.

--- native/main.c
+++ native/main.c
@@ -11,10 +11,17 @@ int main ()
     sqlite3_stmt * stmt;
     int i;
 
-    CALL_SQLITE (open ("test.db", & db));
-    sql = "INSERT INTO t (xyz) VALUES (?)";
+    CALL_SQLITE (open ("db/test.sqlite", & db));
+
+	sql = "DROP TABLE \"employees\"";
+	CALL_SQLITE(exec(db,sql,0,0,0));
+	
+	sql = "CREATE TABLE \"employees\" (\"id\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \"token\" integer(8), \"name\" varchar(20) NOT NULL)";
+	CALL_SQLITE(exec(db,sql,0,0,0));
+
+    sql = "INSERT INTO Employees(name, token) VALUES('employee-1', ?)";
     CALL_SQLITE (prepare_v2 (db, sql, strlen (sql) + 1, & stmt, NULL));
-    CALL_SQLITE (bind_text (stmt, 1, "fruit", 6, SQLITE_STATIC));
+    CALL_SQLITE (bind_int64 (stmt, 1, 4907021672125087744 + 100));
     CALL_SQLITE_EXPECT (step (stmt), DONE);
     printf ("row id was %d\n", (int) sqlite3_last_insert_rowid (db));
     return 0;
